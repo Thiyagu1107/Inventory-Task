ffc3c745c879a197802c18a8dd5211c8519b7185
Thiyagu1107 <97097300+Thiyagu1107@users.noreply.github.com>
2025-06-17 15:10:09 +0530

    r

--- backend/index.js
+++ backend/index.js
@@ -9,13 +9,15 @@ dotenv.config();
 
 connectDB();
 
+const app = express();
+
 app.use(cors({
-  origin: ["http://localhost:3000", "https://inventory-task.onrender.com"],
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  origin: (origin, callback) => {
+    callback(null, true); 
+  },
   credentials: true,
 }));
 
-// app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 
